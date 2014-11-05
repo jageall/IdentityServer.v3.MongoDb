4dd40e4b4483fd2e1fadf46cba4b93d27e7a5ade
James Geall <jageall@gmail.com>
2014-11-05 17:49:18 +0000

    Initial factory created

---
+++ Source/Core.MongoDb/ServiceFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace IdentityServer.Core.MongoDb
+{
+    public class ServiceFactory
+    {
+        private MongoDatabase _db;
+
+        public ServiceFactory(): this("mongodb://localhost")
+        {}
+        public ServiceFactory(string mongoUrl):this(DefaultSettings(mongoUrl), 
+            DefaultStoreSettings())
+        {
+            
+        }
+        public ServiceFactory(MongoClientSettings settings, StoreSettings storeSettings)
+        {
+            var client = new MongoClient(settings);
+            var server = client.GetServer();
+            _db = server.GetDatabase(storeSettings.Database);
+        }
+
+        public static MongoClientSettings DefaultSettings(string mongoUrl)
+        {
+            return new MongoClientSettings
+            {
+                GuidRepresentation = GuidRepresentation.Standard,
+                Server = new MongoServerAddress(mongoUrl)
+            };
+        }
+
+        public static StoreSettings DefaultStoreSettings()
+        {
+            return new StoreSettings
+            {
+                Database = "identityserver", 
+                ClientCollection = "clients"
+            };
+        }
+    }
+}
---
+++ Source/Core.MongoDb/StoreSettings.cs
@@ -0,0 +1,8 @@
+namespace IdentityServer.Core.MongoDb
+{
+    public class StoreSettings
+    {
+        public string Database { get; set; }
+        public string ClientCollection { get; set; }
+    }
+}
