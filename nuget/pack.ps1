86ce5f03f780ed80123790e12a832f2a256b668c
James Geall <jageall@gmail.com>
2014-11-06 17:04:23 +0000

    Resolves #4 Nuget feed added to build script

--- nuget/pack.ps1
+++ nuget/pack.ps1
@@ -1,6 +1,6 @@
 
 $v = new-object System.Version($env:APPVEYOR_BUILD_VERSION)
-$version = "{0}.{1}.{2}-alpha-{3}" -f ($v.Major, $v.Minor, $v.Revision, $env:APPVEYOR_BUILD_NUMBER)
+$version = "{0}.{1}.{2}-alpha-{3}" -f ($v.Major, $v.Minor, $v.Build, $v.Revision)
 
 nuget pack core.nuspec -version $version
 nuget pack AdminModule.nuspec -version $version
