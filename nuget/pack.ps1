be8a4e3ce1242e8a33788defbe200abe91b0daa7
James Geall <jageall@gmail.com>
2014-11-06 16:38:33 +0000

    Fixing semver format

--- nuget/pack.ps1
+++ nuget/pack.ps1
@@ -1,5 +1,6 @@
 
-$version = $env:APPVEYOR_BUILD_VERSION + "-alpha-" + $env:APPVEYOR_BUILD_NUMBER
+$v = new-object System.Version($env:APPVEYOR_BUILD_VERSION)
+$version = + "{0}.{1}.{2}-alpha-{4}" -f ($v.Major, $v.Minor, $v.Revision, $env:APPVEYOR_BUILD_NUMBER)
 
 nuget pack core.nuspec -version $version
 nuget pack AdminModule.nuspec -version $version
