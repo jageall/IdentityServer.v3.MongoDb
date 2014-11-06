0f6247a75a0149283186a0f3dcdf24d5c97c8335
James Geall <jageall@gmail.com>
2014-11-06 15:37:54 +0000

    Adding nuget package restore

--- nuget/pack.ps1
+++ nuget/pack.ps1
@@ -1,4 +1,5 @@
-param($version="invalid")
 
-nuget pack ..\core.nuspec -version $version
-nuget pack ..\AdminModule.nuspec -version $version
+$version = $env(APPVEYOR_BUILD_VERSION) + "alpha-" + $env(APPVEYOR_BUILD_NUMBER)
+
+nuget pack core.nuspec -version $version
+nuget pack AdminModule.nuspec -version $version
