9cf52032b2c744648519db55241966382ed03fff
James Geall <jageall@gmail.com>
2014-11-12 11:19:55 +0000

    Updated to only deploy on tags, builds on all branches

--- nuget/pack.ps1
+++ nuget/pack.ps1
@@ -1,5 +1,4 @@
-$v = new-object System.Version($env:APPVEYOR_BUILD_VERSION)
-$version = "{0}.{1}.{2}-alpha-{3}" -f ($v.Major, $v.Minor, $v.Build, $v.Revision)
-
-nuget pack core.nuspec -version $version
-nuget pack AdminModule.nuspec -version $version
+if($env:APPVEYOR_REPO_TAG -eq 'True'){
+    nuget pack core.nuspec -version $env:APPVEYOR_REPO_BRANCH
+    nuget pack AdminModule.nuspec -version $env:APPVEYOR_REPO_BRANCH
+}
