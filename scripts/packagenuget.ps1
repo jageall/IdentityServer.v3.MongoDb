1d4ec6e90d2532fd9090240d7b8a706fbf5afdc8
James Geall <jageall@gmail.com>
2014-11-14 08:15:18 +0000

    Nuget packages created every build for inspection before publishing

--- scripts/packagenuget.ps1
+++ scripts/packagenuget.ps1
@@ -4,6 +4,6 @@ $version = "{0}.{1}.{2}-alpha-{3}" -f ($v.Major, $v.Minor, $v.Revision, $v.Build
 if($env:APPVEYOR_REPO_TAG -eq 'True'){
   $version = $env:APPVEYOR_REPO_BRANCH
 }
-
+#Nuget packages created every build for inspection before publishing
 nuget pack core.nuspec -version $version
-nuget pack AdminModule.nuspec -version $env:APPVEYOR_REPO_BRANCH
+nuget pack AdminModule.nuspec -version $version
