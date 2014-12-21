40ab89bdef9e2f4657853b97621d5b2c98f687c3
James Geall <jageall@gmail.com>
2014-12-21 21:14:30 +0000

    Fixing private feed version numbers so they match semver

--- scripts/packagenuget.ps1
+++ scripts/packagenuget.ps1
@@ -1,5 +1,5 @@
 $v = new-object System.Version($env:APPVEYOR_BUILD_VERSION)
-$version = "{0}.{1}.{2}-alpha-{3}" -f ($v.Major, $v.Minor, $v.Revision, $v.Build)
+$version = "{0}.{1}.{2}-alpha-{3}" -f ($v.Major, $v.Minor, $v.Build, $v.Revision)
 
 if($env:APPVEYOR_REPO_TAG -eq 'True'){
   $version = $env:APPVEYOR_REPO_BRANCH
