0ac8cbc855a6c7fcee55a7042002bada51b22e57
James Geall <jageall@gmail.com>
2014-12-21 14:20:45 +0000

    Removed manual publish of packages as they are being uploaded later in the script

--- scripts/packagenuget.ps1
+++ scripts/packagenuget.ps1
@@ -7,10 +7,3 @@ if($env:APPVEYOR_REPO_TAG -eq 'True'){
 #Nuget packages created every build for inspection before publishing
 nuget pack core.nuspec -version $version
 nuget pack AdminModule.nuspec -version $version
-
-#Publish packages to project feed
-$core = 'IdentityServer.v3.MongoDb.' + $version + '.nupkg'
-$admin = 'IdentityServer.MongoDb.AdminModule.' + $version + '.nupkg'
-
-appveyor PushArtifact $core
-#appveyor PushArtifact $admin
