a50f75a7604345693c50684f1b5afb1f995f6523
James Geall <jageall@gmail.com>
2017-01-25 19:30:37 +0200

    Disable test parallelization to fix issues with mongodb driver upsert in tests

--- Source/Core.MongoDb.Tests/Properties/AssemblyInfo.cs
+++ Source/Core.MongoDb.Tests/Properties/AssemblyInfo.cs
@@ -15,6 +15,7 @@
  */
 using System.Reflection;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -52,3 +53,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
